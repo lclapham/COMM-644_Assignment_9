b0d19e3cde86ad4a90b3fde4233580cf46dfb80a
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-25 12:36:05 -0700

    Starting the basic layout

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Setting up pre-conditions for accounts
+let userOneAccountBalance = 241.67;
+let userTwoAccountBalance = 1241.67;
+
+
+// User account names to display
+let user1 = "John Doe";
+let user2 = "Jane Doe";
+let userSelection = sessionStorage.getItem("userChoice");
+
+// Set the select
+
+
+// Run the program---
+window.addEventListener('load', () => {
+   console.log(userSelection)
+        
+});
+
+
