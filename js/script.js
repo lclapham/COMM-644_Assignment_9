feb82bf6cb952022c2925b133cca414b4f05a173
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-25 13:23:44 -0700

    Setting up table data

--- js/script.js
+++ js/script.js
@@ -1,20 +1,80 @@
 // Setting up pre-conditions for accounts
-let userOneAccountBalance = 241.67;
-let userTwoAccountBalance = 1241.67;
+let userOneAccountBalance = [["01/20/2020", "deposit", 200.00, "", "", 200.00],
+["01/21/2020", "", "", "withdraw", 20.00, 180.00]];
+
+let userTwoAccountBalance = [["08/20/2020", "deposit", 500.00, "", "", 500.00],
+["08/21/2020", "", "", "withdraw", 200.00, 300.00]];
 
 
 // User account names to display
 let user1 = "John Doe";
 let user2 = "Jane Doe";
 let userSelection = sessionStorage.getItem("userChoice");
+let tr;
+let idNum;
 
 // Set the select
 
 
 // Run the program---
 window.addEventListener('load', () => {
-   console.log(userSelection)
-        
+    console.log(userSelection)
+
 });
+
+// Creates programmtic approach to rows 
+function createRows() {
+    // Create the row element
+    let tr = document.createElement('tr');
+
+    //set tr id with iterated number/use as a reference 
+    tr.id = "row" + idNum;
+
+    // Insert row in 'myTable' on document
+    myTable.appendChild(tr);
+    
+    createTableData( tr.id, 0, userOneAccountBalance);
+    
+
+}
+
+
+// function createTableData(inventory, idNum) {
+function createTableData(tr, idNum, arr) {
+
+    // Setup Table Data
+    let date = document.createElement('td');
+    let deposit = document.createElement('td');
+    let amount1 = document.createElement('td');
+    let withdraw = document.createElement('td');
+    let amount2 = document.createElement('td');
+    let balance = document.createElement('td');
+
+    // Get the specific table row
+   tr = document.getElementById(tr);
+//    tr = document.getElementById(tr);
+
+
+    // Add the html from the inventory array
+
+    date.innerHTML = arr[idNum][0];
+    deposit.innerHTML = arr[idNum][1];
+    amount1.innerHTML = arr[idNum][2];
+    withdraw.innerHTML = arr[idNum][3];
+    amount2.innerHTML = arr[idNum][4];
+    balance.innerHTML = arr[idNum][5];
+
+
+
+    // Append the 'myTable'-->Row with populated TDs
+    tr.appendChild(date);
+    tr.appendChild(deposit);
+    tr.appendChild(amount1);
+    tr.appendChild(withdraw);
+    tr.appendChild(amount2);
+    tr.appendChild(balance);
+
+    
+}
 
 
