3393934ebf8b6c98f138b640f7cdcf25e6ed6572
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-25 13:52:55 -0700

    Updated create rows function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Setting up pre-conditions for accounts
-let userOneAccountBalance = [["01/20/2020", "deposit", 200.00, "", "", 200.00],
-["01/21/2020", "", "", "withdraw", 20.00, 180.00]];
+let userOneAccountBalance = [["01/20/2020", "deposit", 200.00, "", 0.00, 200.00],
+["01/21/2020", "", 0.00, "withdraw", 20.00, 180.00]];
 
-let userTwoAccountBalance = [["08/20/2020", "deposit", 500.00, "", "", 500.00],
-["08/21/2020", "", "", "withdraw", 200.00, 300.00]];
+let userTwoAccountBalance = [["08/20/2020", "deposit", 500.00, "", 0.00, 500.00],
+["08/21/2020", "", 0.00, "withdraw", 200.00, 300.00]];
 
 
 // User account names to display
@@ -23,21 +23,23 @@ window.addEventListener('load', () => {
 });
 
 // Creates programmtic approach to rows 
-function createRows() {
-    // Create the row element
+function createRows(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        // Create the row element
     let tr = document.createElement('tr');
 
     //set tr id with iterated number/use as a reference 
-    tr.id = "row" + idNum;
+    tr.id = "row" + i;
 
     // Insert row in 'myTable' on document
     myTable.appendChild(tr);
-    
+
     createTableData( tr.id, 0, userOneAccountBalance);
+
+    }
     
-
+    
 }
-
 
 // function createTableData(inventory, idNum) {
 function createTableData(tr, idNum, arr) {
@@ -52,19 +54,15 @@ function createTableData(tr, idNum, arr) {
 
     // Get the specific table row
    tr = document.getElementById(tr);
-//    tr = document.getElementById(tr);
-
 
     // Add the html from the inventory array
 
     date.innerHTML = arr[idNum][0];
     deposit.innerHTML = arr[idNum][1];
-    amount1.innerHTML = arr[idNum][2];
+    amount1.innerHTML = arr[idNum][2].toFixed(2);
     withdraw.innerHTML = arr[idNum][3];
-    amount2.innerHTML = arr[idNum][4];
-    balance.innerHTML = arr[idNum][5];
-
-
+    amount2.innerHTML = arr[idNum][4].toFixed(2);
+    balance.innerHTML = arr[idNum][5].toFixed(2);
 
     // Append the 'myTable'-->Row with populated TDs
     tr.appendChild(date);
@@ -73,7 +71,6 @@ function createTableData(tr, idNum, arr) {
     tr.appendChild(withdraw);
     tr.appendChild(amount2);
     tr.appendChild(balance);
-
     
 }
 
